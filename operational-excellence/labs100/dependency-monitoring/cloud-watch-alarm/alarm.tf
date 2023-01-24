2d7c3998f36ce5cc7ae9cba47c1bf75b356e4664
amritlc <amritlc77@gmail.com>
2023-01-24 15:17:03 +0545

    cloudwatch alarm for ec2 instance

---
+++ operational-excellence/labs100/dependency-monitoring/cloud-watch-alarm/alarm.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_metric_alarm" "WA-Lab-DataReadFunction" {
+  alarm_name                = "WA-Lab-DataReadFunction"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+  dimensions = {
+    InstanceId = aws_instance.linux.id
+  }
+  alarm_actions = ["arn:aws:automate:us-west-1:ec2:stop"]
+}
